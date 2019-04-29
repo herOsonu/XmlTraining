49a1d029161f7aa145d4a157e65776838c4e0e91
herOsonu <zhang984408644@live.com>
2019-04-29 17:01:33 +0800

    alter

--- 19.4.29/Demo.java
+++ 19.4.29/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
         XmlBuild.build("F:/users.xml");
         XmlBuild.signUp("zhang","zhang123");
         XmlBuild.signIn("zhang","zhang123");
-        XmlBuild.signOut("zhang");
+        //XmlBuild.signOut("zhang");
         XmlBuild.userCount();
     }
 }
--- 19.4.29/XmlBuild.java
+++ 19.4.29/XmlBuild.java
@@ -3,43 +3,41 @@ import org.dom4j.*;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
-import org.junit.*;
 
 import java.io.*;
 import java.util.Iterator;
 import java.util.List;
-import java.util.WeakHashMap;
 
 
 public class XmlBuild {
 
     private static SAXReader saxReader=new SAXReader();
+    private static String PATH="F:\\users.xml";
+    //写入
+    public static void functionWrite(String path,Document document)throws Exception{
+        Writer fileWriter = new FileWriter(PATH);
+        OutputFormat format=OutputFormat.createPrettyPrint();
+        format.setEncoding("utf-8");
+        XMLWriter xmlWriter = new XMLWriter(fileWriter,format);
+        xmlWriter.write(document);
+        xmlWriter.close();
+    }
 
     //生成xml文件(创建xml文件，初始化user节点，建立一个用户)
-    public static void build(String path) {
-        try {
+    public static void build(String path)throws Exception {
             Document document = DocumentHelper.createDocument();
             Element root = document.addElement("users");
             Element nameElement = root.addElement("name");
             nameElement.addAttribute("name","zhangsan");
             nameElement.addAttribute("password","zhang123");
             nameElement.addAttribute("status","offline");
-            Writer fileWriter = new FileWriter(path);
-            OutputFormat format=OutputFormat.createPrettyPrint();
-            format.setEncoding("utf-8");
-            XMLWriter xmlWriter = new XMLWriter(fileWriter,format);
-            xmlWriter.write(document);
-            xmlWriter.close();
-            System.out.println("xml文档创建成功！");
-            System.out.println(" ");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            System.out.println("xml文档创建成功！\n");
+            functionWrite(PATH,document);
     }
 
     //读取xml文件，输出当前用户信息
     public static void xmlLoad() throws Exception {
-        List<Element> list=saxReader.read("F:\\users.xml").getRootElement().elements();
+        List<Element> list=saxReader.read(PATH).getRootElement().elements();
         for(Element l:list){
             Iterator<Element> iterator=l.elementIterator();
             while (iterator.hasNext()){
@@ -53,19 +51,14 @@ public class XmlBuild {
 
     //注册（在user下添加name节点，name下增加password跟status节点，最后写入到xml文档）
     public static void signUp(String name, String passWord) throws Exception {
-        Document document = saxReader.read(new File("F:\\users.xml"));
+        Document document = saxReader.read(new File(PATH));
         Element root = document.getRootElement();
         Element nameElement = root.addElement("name");
         nameElement.addAttribute("name",name);
         nameElement.addAttribute("password",passWord);
         nameElement.addAttribute("status","offline");
-        System.out.println("用户："+name+" 注册成功~");
-        Writer osWrite=new OutputStreamWriter(new FileOutputStream("F:/users.xml"));
-        OutputFormat format = OutputFormat.createPrettyPrint();
-        format.setEncoding("UTF-8");
-        XMLWriter writer = new XMLWriter(osWrite,format);
-        writer.write(document);
-        writer.close();
+        System.out.println("用户："+name+" 注册成功~\n");
+        functionWrite(PATH,document);
     }
 
 
@@ -80,46 +73,41 @@ public class XmlBuild {
                 if(element.attributeValue("status").equals("offline")){
                     Attribute attribute=element.attribute("status");
                     attribute.setValue("online");
-                    Writer osWrite=new OutputStreamWriter(new FileOutputStream("F:/users.xml"));
-                    OutputFormat format = OutputFormat.createPrettyPrint();
-                    format.setEncoding("UTF-8");
-                    XMLWriter writer = new XMLWriter(osWrite,format);
-                    writer.write(document);
-                    writer.close();
-                    System.out.println("user:"+name+" SignIn succeed!");
+                    functionWrite(PATH,document);
+                    System.out.println("user:"+name+" SignIn succeed!\n");
                 }else{
-                    System.out.println("user:"+name+" already SignIn");
+                    System.out.println("user:"+name+" already SignIn\n");
                 }
                 return;
             }
         }
-        System.out.println("SignIn failed!");
+        System.out.println("SignIn failed!\n");
     }
 
     //退出登陆
     public static void signOut(String name) throws Exception {
-        Document document=saxReader.read("F:\\users.xml");
+        Document document=saxReader.read(PATH);
         Iterator it = document.getRootElement().elementIterator();
         while (it.hasNext()) {
             Element element = (Element) it.next();
             if (element.attributeValue("name").equals(name)&&element.attributeValue("status").equals("online")){
-                System.out.println("user:"+name+" SignOut succeed！");
+                System.out.println("user:"+name+" SignOut succeed！\n");
                 Attribute attribute=element.attribute("status");
                 attribute.setValue("offline");
-                FileOutputStream fileOutputStream=new FileOutputStream("F:\\users.xml");
+                FileOutputStream fileOutputStream=new FileOutputStream(PATH);
                 XMLWriter writer=new XMLWriter(fileOutputStream);
                 writer.write(document);
                 writer.close();
                 return;
             }
         }
-        System.out.println("用户："+name+" 未登录");
+        System.out.println("用户："+name+" 未登录\n");
     }
 
     //输出当前用户数量
     public static void userCount()throws Exception{
-        List<Element> list = saxReader.read("F:\\users.xml").getRootElement().elements();
-        System.out.println("用户数量："+list.size());
+        List<Element> list = saxReader.read(PATH).getRootElement().elements();
+        System.out.println("用户数量："+list.size()+"\n");
     }
 }
 
